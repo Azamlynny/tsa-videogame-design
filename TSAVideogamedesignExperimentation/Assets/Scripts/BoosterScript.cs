876e174afe5f82cdff1c3cbc960fc2089995766f
Azamlynny <azamlynny@hotmail.com>
2021-01-26 23:15:06 -0500

    Added booster

---
+++ TSAVideogamedesignExperimentation/Assets/Scripts/BoosterScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoosterScript : MonoBehaviour
+{
+
+    public float boosterStrength = 200f;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay()
+    {
+        player.GetComponent<Rigidbody>().AddForce(player.GetComponent<PlayerMovementScript>().planetToPlayerRay * boosterStrength);
+    }
+}
